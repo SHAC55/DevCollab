0eb8c3a12607e81dc632bbd3f5d6e908f5274135
SAIF CHAUDHARY <SAIFCHOUDHARY.0666@GMAIL.COM>
2026-01-22 19:34:37 +0530

    fixed url deployment issue 1.0

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,7 @@
 import { io } from 'socket.io-client'
 
-export  const socket  = io("http://localhost:5000",{
+export  const socket  = io("https://devcollab-ov0h.onrender.com",{
     withCredentials:true
 })
+
+// http://localhost:5000
