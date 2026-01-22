6a0eb5a74a2e70eee65657f7962d71814b80acbf
SAIF CHAUDHARY <SAIFCHOUDHARY.0666@GMAIL.COM>
2026-01-22 20:05:19 +0530

    fixed url deployment issue 1.4

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,12 +1,20 @@
-// src/routes/ProtectedRoute.js
-import React, { useContext } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../context/authContext";
 
 const ProtectedRoute = () => {
-  const { token } = useAuth;
+  const { token, loading } = useAuth();
+  const location = useLocation();
 
-  return token ? <Outlet /> : <Navigate to="/sign-in" />;
+  // while checking token (important on refresh)
+  if (loading) return null; // or loader
+
+  // if token exists → allow route
+  if (token) {
+    return <Outlet />;
+  }
+
+  // if not logged in → redirect to sign-in
+  return <Navigate to="/sign-in" replace state={{ from: location }} />;
 };
 
 export default ProtectedRoute;
